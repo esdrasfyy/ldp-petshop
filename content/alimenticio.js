0da7f39a029425d13d53bca544a2eb05c79ac8f8
Fernando Esdras da Silva <contatoesdrasoficial@gmail.com>
2024-06-06 03:22:48 -0300

    update in carousel

---
+++ content/alimenticio.js
@@ -0,0 +1,278 @@
+export const alimenticio = [
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+];
+
+export const vestuario = [
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+];
+
+export const outros = [
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+  {
+    title: "HILL´S CANINE AD 7+ PEQ E MINI 800G",
+    price: "64,49",
+    image: "./assets/cao-alegre-a-divertir-se.jpg",
+  },
+];
